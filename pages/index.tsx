7eff2517bc6c44fd31a1b56a963e82f02b6ba9fe
akhil272 <akhilms272@gmail.com>
2021-10-01 18:11:32 +0530

    styles used for h n w. rest completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ function Homepage() {
   return (
     <div className="min-h-screen grid place-items-center p-4 bg-gray-900">
       <div
-        className=" relative bg-white overflow-hidden"
+        className="relative bg-white overflow-hidden "
         style={{ width: "640px", height: "360px" }}
       >
         <div
@@ -30,6 +30,37 @@ function Homepage() {
           className="bg-purple-200 absolute rounded-full -top-20 -right-56"
         ></div>
 
+        <div className="absolute top-8 right-8 ">
+          <img
+            className="w-64 h-36 object-cover rounded-2xl shadow-2xl transform rotate-3"
+            src="https://www.dropbox.com/s/6lipca71meyf9f2/image-1.jpg?raw=1"
+            alt="YouTuber cheerfully giving a shaka"
+          />
+          <img
+            className="w-64 h-36 object-cover rounded-2xl shadow-2xl transform -rotate-3 translate-x-16 -translate-y-2"
+            src="https://www.dropbox.com/s/sox1969sc1o6my9/image-2.png?raw=1"
+            alt="Screenshot of Tailwind CSS code"
+          />
+        </div>
+        <div className="absolute right-12 inset-y-0 grid items-center">
+          <a
+            className="w-12 h-12 bg-purple-500 hover:bg-purple-400 transition rounded-full ring-4 ring-white grid place-items-center"
+            href="#"
+          >
+            <span className="sr-only">Watch the video</span>
+            <svg
+              className="ml-1 w-4"
+              viewBox="0 0 16 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 7.26795C16.3333 8.03775 16.3333 9.96225 15 10.7321L3 17.6603C1.66667 18.4301 1.01267e-06 17.4678 1.07997e-06 15.9282L1.68565e-06 2.0718C1.75295e-06 0.532196 1.66667 -0.430054 3 0.339746L15 7.26795Z"
+                fill="white"
+              />
+            </svg>
+          </a>
+        </div>
         <div className="flex flex-col relative p-8 h-full justify-end space-y-4 w-2/3">
           <p className="uppercase font-semibold text-sm text-purple-600 tracking-wider">
             How we build it
